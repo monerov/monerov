e4d8ee393d30d3f4667495c3f20503750641259a
miltonf <miltonf@monerov.org>
2019-01-05 13:37:12 +0200

    add multisig transaction, commit transaction, sign multisig transaction and get multisig data

--- src/wallet/wallet2.cpp
+++ src/wallet/wallet2.cpp
@@ -5048,7 +5048,7 @@ bool wallet2::save_multisig_tx(const multisig_tx_set &txs, const std::string &fi
   return epee::file_io_utils::save_string_to_file(filename, ciphertext);
 }
 //----------------------------------------------------------------------------------------------------
-std::string wallet2::save_multisig_tx(const std::vector<pending_tx>& ptx_vector)
+wallet2::multisig_tx_set wallet2::make_multisig_tx_set(const std::vector<pending_tx>& ptx_vector) const
 {
   multisig_tx_set txs;
   txs.m_ptx = ptx_vector;
@@ -5061,7 +5061,12 @@ std::string wallet2::save_multisig_tx(const std::vector<pending_tx>& ptx_vector)
 
   txs.m_signers.insert(get_multisig_signer_public_key());
 
-  return save_multisig_tx(txs);
+  return txs;
+}
+
+std::string wallet2::save_multisig_tx(const std::vector<pending_tx>& ptx_vector)
+{
+  return save_multisig_tx(make_multisig_tx_set(ptx_vector));
 }
 //----------------------------------------------------------------------------------------------------
 bool wallet2::save_multisig_tx(const std::vector<pending_tx>& ptx_vector, const std::string &filename)
